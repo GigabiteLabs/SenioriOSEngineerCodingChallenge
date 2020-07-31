2c48783515a18ca5570d2273efa0825c3e54a118
DanBurkhardt <dan@gigabitelabs.com>
2020-07-31 19:18:08 -0400

    added single test class to avoid codebeat error

---
+++ senior-dev-submission/Classes/TestClass.swift
@@ -0,0 +1,14 @@
+// TestClass.swift
+//
+// Created by GigabiteLabs on 7/31/20
+// Swift Version: 5.0
+// Copyright © 2020 GigabiteLabs. All rights reserved.
+//
+
+import Foundation
+
+public class TestClass {
+    init() {
+        print("initialized")
+    }
+}
